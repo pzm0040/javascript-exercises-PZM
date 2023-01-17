7ff318b7ff43de09923cb40f0245dba39cc54146
pzm0040 <patrickmcardell@gmail.com>
2023-01-17 13:07:09 -0500

    Completed exercises 01 and 02 with all tests=Pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let str = string;
+    let multiStr = "";
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        for (i=0; i < num; i++) {
+        multiStr += str;
+    }}
+    return multiStr;
 };
 
 // Do not edit below this line
