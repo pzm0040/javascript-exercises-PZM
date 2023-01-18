5dd90fa32798a3d923279f2db2dc6cea10579e99
pzm0040 <patrickmcardell@gmail.com>
2023-01-18 13:28:50 -0500

    Completed exercise #3 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,21 @@
-const reverseString = function() {
+// 1. split string into array 2.
 
+
+const reverseString = function(str) {
+    const strOne = str;
+    let strArray = strOne.split('');
+    let lenArray = strArray.length;
+    let i = lenArray - 1;
+    let reverseArray = [];
+    if (lenArray < 1){
+        reverseArray = '';
+    } else {
+        while (i >= 0){
+            reverseArray += strArray[i];
+            i--;
+        }
+    }
+    return reverseArray;
 };
 
 // Do not edit below this line
