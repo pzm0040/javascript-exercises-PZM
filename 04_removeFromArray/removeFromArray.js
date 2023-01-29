02cad722665337486682d7b70912a384be4042bc
pzm0040 <patrickmcardell@gmail.com>
2023-01-28 20:52:46 -0500

    solution for exercise 4 remove item from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(theArray, ...args) {
+  // creating empty arry, will return later
+  const newArr = [];
+  theArray.forEach((item) => {
+    if (!args.includes(item)) {
+      // adding to the new array if != args
+      newArr.push(item);
+    }
+  });
+  return newArr;
 };
 
 // Do not edit below this line
